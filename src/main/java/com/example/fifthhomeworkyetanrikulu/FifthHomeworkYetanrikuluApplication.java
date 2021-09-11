29d55177097d1a3ab93aedc688c4d2e79de3cc5b
Yaemta <yaemta@gmail.com>
2021-09-11 22:51:12 +0300

    first commit

---
+++ src/main/java/com/example/fifthhomeworkyetanrikulu/FifthHomeworkYetanrikuluApplication.java
@@ -0,0 +1,13 @@
+package com.example.fifthhomeworkyetanrikulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifthHomeworkYetanrikuluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifthHomeworkYetanrikuluApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fifthhomeworkyetanrikulu/FifthHomeworkYetanrikuluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fifthhomeworkyetanrikulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifthHomeworkYetanrikuluApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
